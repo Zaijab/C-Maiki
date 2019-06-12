14be6a086877116b6c945d29a6e1fb9d34ce49f2
Zain Jabbar <zaijab2000@gmail.com>
2019-06-12 12:39:55 -1000

    More Tests on Manifolds

---
+++ manifold/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+matrix = [[1,2,3],[1,2,3]]
+
+np.save("matrix.txt",matrix)
---
+++ metrics/hamming.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import numpy as np
+
+f = open("../data/genetic/16S/sequences_97.fasta")
+all_otu_data = f.read()
+
+otu_list = all_otu_data.split(">")
+
+otu_dna_pair = []
+
+for otu in otu_list:
+    otu_dna_pair.append(otu.split("\n"))
+
+def hamming_dist(otu1, otu2):
+    count = 0
+    for i in range(len(otu1)):
+        if otu1[i] != otu2[i]:
+            count += 1
+    return count
+
+
+def calc_dist_matrix(otu_dna_pair_list):
+    dist_matrix = [[0 for x in range(len(otu_dna_pair_list))] for y in range(len(otu_dna_pair_list))]
+    for i in range(len(otu_dna_pair_list)):
+        for j in range(len(otu_dna_pair_list)):
+            dist_matrix[i][j] = hamming_dist(otu_dna_pair_list[i][1],otu_dna_pair_list[j][1])
+            # print(dist_matrix[i][j])
+    return dist_matrix
+
+hamming_dist_matrix = calc_dist_matrix(otu_dna_pair[1:])
+
+np.save("hamming_dist_matrix",hamming_dist_matrix)
+print(hamming_dist_matrix)
