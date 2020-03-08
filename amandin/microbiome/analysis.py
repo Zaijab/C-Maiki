80617151596b5ea946427064022a711875a28b9d
Zain Jabbar <zaijab2000@gmail.com>
2020-03-07 18:57:13 -1000

    Cleaned up analysis pipeline, leading into spectral clustering

--- amandin/microbiome/analysis.py
+++ amandin/microbiome/analysis.py
@@ -1,124 +1,21 @@
-# Import dependencies
-import h5py
-import numpy as np
-import pickle
-import time
-import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.spatial.distance import pdist, squareform
+from sklearn.cluster import SpectralClustering
 
-# Initialize Files and Paths
-otu_data_path = "../data/microbiome/zain/abundance_table_97.shared"
+# Import Data
+abundance_table_path = "./amandin/data/microbiome/zain/abundance_table_100.shared"
 
-# Import Relevent Data from files
+with open(abundance_table_path, "r") as file_literal:
+    raw_abundance_data = [line.strip().split("\t") for line in file_literal]
+    otu_names = raw_abundance_data[0][3:]
+    sample_names = [line[1] for line in raw_abundance_data[1:]]
+    otu_counts = [line[3:] for line in raw_abundance_data[1:]]
 
+abundance_table = pd.DataFrame(
+    data=otu_counts, index=sample_names, columns=otu_names)
 
-def import_otu_data(filename='../data/microbiome/zain/abundance_table_97.shared', load_data=False):
-    """ Import abundance data from file. sample_data is a list of strings of
-    each sample name ['100001', '100002', etc.]. otu_data is a matrix of
-    abundances, with otu_data[i] corresponding to sample i and consisting of a
-    vector of OTUs."""
-
-    pickle_filename = f'{filename}.pi'
-    if load_data:
-        with open(pickle_filename, 'rb') as f:
-            sample_data, raw_otu_data = pickle.load(f)
-        return sample_data, raw_otu_data
-
-    with open(filename, 'r') as f:
-        raw_otu_data = [line.strip().split("\t") for line in f][1:]
-    sample_data = [val[1] for val in raw_otu_data]
-    raw_otu_data = [[int(x) for x in line[3:]] for line in raw_otu_data]
-    raw_otu_data = np.array(raw_otu_data)
-
-    with open(pickle_filename, 'wb') as f:
-        pickle.dump((sample_data, raw_otu_data), f)
-
-    return sample_data, raw_otu_data
-
-
-def import_metadata(filename='../data/genetic/CMAIKI_Miseq1MappingFile_20181105.csv', load_data=False):
-    """ Import metadata from file. Calling metadata['Otu00001'] will return
-    'Plant' or 'Fungus', etc. """
-
-    pickle_filename = f'{filename}.pi'
-    if load_data:
-        with open(pickle_filename, 'rb') as f:
-            metadata = pickle.load(f)
-        return metadata
-
-    with open(filename, 'r') as f:
-        raw_metadata = [line.strip().split(",") for line in f][1:]
-    metadata = {}
-    for line in raw_metadata:
-        metadata[line[0]] = line[17]
-
-    with open(pickle_filename, 'wb') as f:
-        pickle.dump(metadata, f)
-
-    return metadata
-
-
-def import_taxonomy(filename='../data/genetic/16S/annotations_97.taxonomy', load_data=False):
-    """ Import OTU taxonomy from file. otu_names is a list of strings of each
-    OTU ['Otu00001', 'Otu00002', 'Otu00003', etc.]. taxonomies is a dictionary
-    with otu_names as the keys and values a list of taxonomic hierarchies, e.g.
-    taxonomies['Otu00001'] = ['Bacteria', 'Bacterioides', etc.]. taxonomic_tree
-    is a Tree class from the skbio module that contains all taxonomic
-    information"""
-
-    pickle_filename = f'{filename}.pi'
-    if load_data:
-        with open(pickle_filename, 'rb') as f:
-            otu_names, taxonomies, taxonomic_tree = pickle.load(f)
-        return otu_names, taxonomies, taxonomic_tree
-
-    with open(filename, 'r') as f:
-        raw_metadata = [line.strip().split("\t") for line in f][1:]
-    taxonomies = {}
-    otu_names = []
-    for line in raw_metadata:
-        taxonomies[line[0]] = line[2].split(";")[:-1]
-        otu_names.append(line[0])
-
-    taxonomic_tree = skbio.TreeNode.from_taxonomy(
-        [(x, taxonomies[x]) for x in taxonomies])
-    taxonomic_tree = taxonomic_tree.root_at(taxonomic_tree)
-    for node in taxonomic_tree.traverse():
-        node.length = 1
-    with open(pickle_filename, 'wb') as f:
-        pickle.dump((otu_names, taxonomies, taxonomic_tree), f)
-    return otu_names, taxonomies, taxonomic_tree
-
-
-def get_host_distribution(sample_names, otu_data, metadata, verbose=False):
-    """ Update metadata to include sample_names that weren't explicitly provided in
-    the metadata file. If verbose, print the distribution of hosts. """
-    hosts = {}
-    count = 0
-    for sample in sample_names:
-        try:
-            metadata[sample]
-        except KeyError:
-            metadata[sample] = 'no metadata'
-
-        try:
-            hosts[metadata[sample]] += 1
-        except KeyError:
-            hosts[metadata[sample]] = 1
-
-    if verbose:
-        print(hosts)
-    return metadata
-
-
-# Import Otu Data and metadata
-data_path = "./amandin/data/microbiome/zain/abundance_table_100.shared"
-sample_names, otu_data = import_otu_data(data_path)
-
-# Start creating Abundance table with metadata
-microbiome = h5py.File("./amandin/microbiome/microbiome.hdf5", "w")
-otu_dataset = microbiome.create_dataset("otu_data", data=otu_data)
-print(microbiome.keys())
-print(microbiome.attrs.keys())
-for key in microbiome.keys():
-    print(microbiome[key])
-microbiome.close()
+# Spectral Clustering
+weights = pd.DataFrame(
+    squareform(pdist(abundance_table, metric="minkowski", p=1)),
+    columns=abundance_table.index,
+    index=abundance_table.index)
