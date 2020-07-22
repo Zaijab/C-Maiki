25a08f64d6e6cfb7c0146bbd8073a8ffd2a97e2b
Zain Jabbar <zaijab2000@gmail.com>
2020-07-22 10:51:53 -1000

    Updated Coronavirus model in C along with better comments to explain the code.

--- amandin/covid/model.c
+++ amandin/covid/model.c
@@ -10,6 +10,15 @@
  * The error value solely depends on the parameters for the model
  * Thus GSL will be called to minimize the error function by finding optimal parameters
  * The optimal parameters can then be used in a simulation, be plotted, and documented.
+ *
+ * The code adapts the model equations by using array indicies to function as subscripts in the model.
+ * This means that E_m(t) in math gets translated to E[m][t] in C.
+ * However arrays are indexed by natural numbers, thus the following key associates groups to natural numbers.
+ * h -> 0
+ * m -> 1
+ * c -> >=2
+ * c (the community group) will be 2 for the time being, but the plans are for the model to expand.
+ * So there can be more community indicies later.
  */
 
 #include <math.h>
@@ -41,23 +50,58 @@ int main(int argc, char** argv){
  * PARAMETERS:
  * -----------
  */
-void model_updater(float S[][], float E[][][], float I[][][], float R[][][], float l[][], float p[], float q[], float h[][], float r, int t) {
+void model_updater(float S[][], float E[][][], float I[][][], float R[][][], int t,
+		   float l[][], float p[], float q[], float h[][], float r) {
+  S[c][t+1] = exp(-l[c][t]) * S[c][t];
+  E[c][1][t+1] = (1 - exp(-l[c][t])) * S[c][t];
+
+  for(int i = 2; i <= 14; i++) {
+    E[c][i][t+1] = (1 - p[i-1]) * E[c][i-1][t];
+  }
+  
+  for(int i = 1; i <= 14; i++) {
+    I[c][1][t+1] += p[i]*(1-q[i])*E[c][i][t];
+  }
+  I[c][2][t+1] = (1-h[c][1])*I[c][1][t];
+  I[c][3][t+1] = (1-h[c][2])*I[c][2][t] + (1-r)(1-h[c][3])*I[c][3][t];
+  for(int j = 4; j <= 5; j++) {
+    I[c][j] = r*(1-h[c][j-1])*I[c][j-1][t] + (1-r)(1-h[c][j])*I[c][j][t];
+  }
+  R[c][t+1] = R[c][t] + r*I[c][5][t]+r*I[m][5][t];
+
   S[h][t+1] = exp(-l[h][t]) * S[h][t];
   E[h][1][t+1] = (1 - exp(-l[h][t])) * S[h][t];
 
-  for(int i = 2; i <= 10; i++) {
+  for(int i = 2; i <= 14; i++) {
     E[h][i][t+1] = (1 - p[i-1]) * E[h][i-1][t];
   }
   
-  for(int i = 1; i <= 10; i++) {
-    I[h][1][t+1] += p[i]*(1-q[i])*E[h][i][t]
+  for(int i = 1; i <= 14; i++) {
+    I[h][1][t+1] += p[i]*(1-q[i])*E[h][i][t];
   }
   I[h][2][t+1] = (1-h[h][1])*I[h][1][t];
   I[h][3][t+1] = (1-h[h][2])*I[h][2][t] + (1-r)(1-h[h][3])*I[h][3][t];
   for(int j = 4; j <= 5; j++) {
     I[h][j] = r*(1-h[h][j-1])*I[h][j-1][t] + (1-r)(1-h[h][j])*I[h][j][t];
   }
-  R[h][t+1] = R[h][t] + r*I[h][5][t]+r*I[m][5][t]
+  R[h][t+1] = R[h][t] + r*I[h][5][t]+r*I[m][5][t];
+
+  for(int i = 2; i <= 14; i++) {
+    E[m][t+1] = (1 - p[i - 1])*(q[i] * E[c][i - 1][t] + E[m][i - 1][t]);
+    }
+
+  I[m][1][t+1] = 0;
+  for(int i = 1; i <= 14; i++) {
+    I[m][1][t+1] += p[i]*(q[i]*E[c][i][t] + E[m][i][t]);
+  }			  
+  I[m][2][t+1] = h[c][1] * I[c][1][t] + h[h][1] * I[h][1][t] + I[m][1][t];
+  I[m][3][t + 1] = r * (h[c][2] * I[c][2][t] + h[h][2] * I[h][2][t] + I[m][2][t])
+    + (1 - r)*(h[c][3] * I[c][3][t] + h[h][3] * I[h][3][t] + I[m][1][t]);
+  
+  for(int j = 4; j <= 5; j++) {
+    I[m][j][t + 1] = r * (h[c][j - 1] * I[c][j - 1][t] + h[h][j - 1] * I[h][j - 1][t] + I[m][j - 1][t])
+      + (1 - r)*(h[c][j] * I[c][j][t] + h[h][j] * I[h][j][t] + I[m][j][t]);
+  }
 }
 
 /* FUNCTION: model_solver
@@ -67,7 +111,11 @@ void model_updater(float S[][], float E[][][], float I[][][], float R[][][], flo
  * PARAMETERS:
  * -----------
  */
-int* model_solver(float l[][][], float p[], float q[], float h[][], float r, int t) {
+void model_solver(int t, float l[][][], float p[], float q[], float h[][], float r) {
+  float S[][];
+  float E[][][];
+  float I[][][];
+  float R[][][];
   return {1};
 }
 
